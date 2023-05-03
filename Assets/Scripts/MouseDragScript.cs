f40fd9a4c8e4a68e64c2d7b6929f4099f26c4fe9
Joe Frayne <joefrayne@gmail.com>
2023-05-02 19:33:58 -0700

    Card pickup and drop supports to and from table and ground smoothly.

--- Assets/Scripts/MouseDragScript.cs
+++ Assets/Scripts/MouseDragScript.cs
@@ -14,37 +14,56 @@ public class MouseDragScript : MonoBehaviour
     public AudioClip cardDropSound;
 
     // Private variables
-    private GameObject groundPlane;
+    private GameObject groundObj;
+    private Plane groundPlane;
+    private GameObject tableObj;
+    private Plane tablePlane;
     private GameObject cardBeam;
-    private Vector3 offset;
-    private bool isDragging = false;
-    private bool isFallingTx = false;
-    private bool isFallingTy = false;
-    private bool isFallingTz = false;
-    private bool isFallingR = false;
+
     private Ray ray;
     private RaycastHit hit;
-    private Plane ground;
+    private Vector3 offset;
     private float distance;
     private Vector3 hitPoint;
+
+    private float startingY;
+    private float startingZ;
+    private float startingBeamY;
     private float targetTx;
     private float targetTy;
     private float targetTz;
+    private float targetBeamTy;
     private Quaternion targetR;
     private float stepTx;
     private float stepTy;
     private float stepTz;
+    private float stepBeamTy;
     private Quaternion stepR;
+
+    private bool isDragging = false;
+    private bool isOverGround = false;
+    private bool isFallingTx = false;
+    private bool isFallingTy = false;
+    private bool isFallingTz = false;
+    private bool isFallingR = false;
     private bool isPlayingAudio = false;
     private bool hasLanded = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        groundPlane = GameObject.Find("Ground Plane");
-        ground = new Plane(Vector3.up, groundPlane.transform.position);
+        groundObj = GameObject.Find("Ground");
+        groundPlane = new Plane(Vector3.up, groundObj.transform.position);
+
+        tableObj = GameObject.Find("Table");
+        tablePlane = new Plane(Vector3.up, tableObj.transform.position);
 
         cardBeam = GameObject.Find("CardBeam");
+
+        startingY = transform.position.y;
+        startingZ = transform.position.z;
+
+        startingBeamY = cardBeam.transform.position.y;
     }
 
     private IEnumerator PlayAudioWithDelayCoroutine(AudioClip clip, float delay)
@@ -76,7 +95,6 @@ public class MouseDragScript : MonoBehaviour
                     // Set the flag to indicate that we're playing audio
                     isPlayingAudio = true;
                     StartCoroutine(PlayAudioWithDelayCoroutine(cardPickupSound, .1f));
-                    Debug.Log("Picked Up");
                 }
             }
         }
@@ -86,18 +104,59 @@ public class MouseDragScript : MonoBehaviour
             if (isDragging)
             {
                 ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if (ground.Raycast(ray, out distance))
+
+                if (Physics.Raycast(ray, out hit))
                 {
-                    // Gradually move above the ground target
-                    hitPoint = ray.GetPoint(distance);
-                    targetTx = hitPoint.x + offset.x;
-                    targetTy = transform.position.y;
-                    targetTz = hitPoint.z + offset.z;
-                    stepTx = transform.position.x + dragFollowingRate * (targetTx - transform.position.x);
-                    stepTy = transform.position.y + dragFollowingRate * (targetTy - transform.position.y);
-                    stepTz = transform.position.z + dragFollowingRate * (targetTz - transform.position.z);
-                    transform.position = new Vector3(stepTx, stepTy, stepTz);
-                    cardBeam.transform.position = new Vector3(stepTx, cardBeam.transform.position.y, stepTz);
+                    if ((isOverGround && hit.transform == tableObj.transform) || (!isOverGround && hit.transform != groundObj.transform))
+                    {
+                        isOverGround = false;
+                        if (tablePlane.Raycast(ray, out distance))
+                        {
+                            // Gradually move above the table target
+                            hitPoint = ray.GetPoint(distance);
+                            targetTx = hit.point.x;
+                            targetTy = startingY + 1.5f;
+                            targetTz = startingZ - .75f;
+                            stepTx = transform.position.x + dragFollowingRate * (targetTx - transform.position.x);
+                            stepTy = transform.position.y + dragFollowingRate * (targetTy - transform.position.y);
+                            stepTz = transform.position.z + dragFollowingRate * (targetTz - transform.position.z);
+                            transform.position = new Vector3(stepTx, stepTy, stepTz);
+                            
+                            targetBeamTy = startingBeamY;
+                            stepBeamTy = cardBeam.transform.position.y + dragFollowingRate * (targetBeamTy - cardBeam.transform.position.y);
+                            cardBeam.transform.position = new Vector3(stepTx, stepBeamTy, stepTz);
+
+                            // Gradually rotate back to starting
+                            targetR = Quaternion.Euler(50, -180, 0);
+                            stepR = Quaternion.Lerp(transform.rotation, targetR, fallingRateRotating);
+                            transform.rotation = stepR;
+                        }
+                    }
+                    else
+                    {
+                        isOverGround = true;
+                        if (groundPlane.Raycast(ray, out distance))
+                        {
+                            // Gradually move above the ground target
+                            hitPoint = ray.GetPoint(distance);
+                            targetTx = hitPoint.x + offset.x;
+                            targetTy = startingY;
+                            targetTz = hitPoint.z + offset.z;
+                            stepTx = transform.position.x + dragFollowingRate * (targetTx - transform.position.x);
+                            stepTy = transform.position.y + dragFollowingRate * (targetTy - transform.position.y);
+                            stepTz = transform.position.z + dragFollowingRate * (targetTz - transform.position.z);
+                            transform.position = new Vector3(stepTx, stepTy, stepTz);
+                            
+                            targetBeamTy = startingBeamY;
+                            stepBeamTy = cardBeam.transform.position.y + dragFollowingRate * (targetBeamTy - cardBeam.transform.position.y);
+                            cardBeam.transform.position = new Vector3(stepTx, stepBeamTy, stepTz);
+
+                            // Gradually rotate back to starting
+                            targetR = Quaternion.Euler(50, -180, 0);
+                            stepR = Quaternion.Lerp(transform.rotation, targetR, fallingRateRotating);
+                            transform.rotation = stepR;
+                        }
+                    }
                 }
             }
 
@@ -116,52 +175,104 @@ public class MouseDragScript : MonoBehaviour
 
             if (isFallingTx || isFallingTy || isFallingTz || isFallingR)
             {
-                // Gradually move down
-                targetTx = hitPoint.x + offset.x;
-                targetTy = groundPlane.transform.position.y + fallingRateDown;
-                targetTz = hitPoint.z + offset.z;
-                stepTx = transform.position.x + fallingRateSliding * (targetTx - transform.position.x);
-                stepTy = transform.position.y + fallingRateDown * (targetTy - transform.position.y);
-                stepTz = transform.position.z + fallingRateSliding * (targetTz - transform.position.z);
-                if (stepTx <= fallingRateSliding)
+                if (!isOverGround)
                 {
-                    stepTx = targetTx;
-                    isFallingTx = false;
-                }
-                if (stepTy <= fallingRateDown + groundPlane.transform.position.y + .01f)
-                {
-                    stepTy = targetTy;
-                    isFallingTy = false;
-                }
-                if (stepTz <= fallingRateSliding)
-                {
-                    stepTz = targetTz;
-                    isFallingTz = false;
-                }
-                transform.position = new Vector3(stepTx, stepTy, stepTz);
-                cardBeam.transform.position = new Vector3(stepTx, stepTy - 5, stepTz);
+                    // Gradually move to the table
+                    targetTx = hit.point.x;
+                    targetTy = startingY;
+                    targetTz = startingZ;
+                    stepTx = transform.position.x + fallingRateSliding * (targetTx - transform.position.x);
+                    stepTy = transform.position.y + fallingRateDown * (targetTy - transform.position.y);
+                    stepTz = transform.position.z + fallingRateSliding * (targetTz - transform.position.z);
+                    if (stepTx <= fallingRateSliding)
+                    {
+                        stepTx = targetTx;
+                        isFallingTx = false;
+                    }
+                    if (stepTy <= fallingRateDown + startingY + .01f)
+                    {
+                        stepTy = targetTy;
+                        isFallingTy = false;
+                    }
+                    if (stepTz <= fallingRateSliding)
+                    {
+                        stepTz = targetTz;
+                        isFallingTz = false;
+                    }
+                    transform.position = new Vector3(stepTx, stepTy, stepTz);
+                    cardBeam.transform.position = new Vector3(stepTx, startingBeamY, stepTz);
 
-                // Gradually rotate flat
-                targetR = Quaternion.Euler(0, -180, 0);
-                float angle = Quaternion.Angle(transform.rotation, targetR);
-                if (angle <= fallingRateRotating)
-                {
-                    stepR = targetR;
-                    isFallingR = false;
+                    // Gradually rotate to starting
+                    targetR = Quaternion.Euler(50, -180, 0);
+                    float angle = Quaternion.Angle(transform.rotation, targetR);
+                    if (angle <= fallingRateRotating)
+                    {
+                        stepR = targetR;
+                        isFallingR = false;
+                    }
+                    else
+                    {
+                        stepR = Quaternion.Lerp(transform.rotation, targetR, fallingRateRotating);
+                    }
+                    transform.rotation = stepR;
+
+                    if (!hasLanded && stepTy <= fallingRateDown + startingY + 1f)
+                    {
+                        // Set the flag to indicate that we're playing audio
+                        isPlayingAudio = true;
+                        StartCoroutine(PlayAudioWithDelayCoroutine(cardDropSound, 0f));
+                        hasLanded = true;
+                        isOverGround = false;
+                    }
                 }
                 else
                 {
-                    stepR = Quaternion.Lerp(transform.rotation, targetR, fallingRateRotating);
-                }
-                transform.rotation = stepR;
+                    // Gradually move down
+                    targetTx = hitPoint.x + offset.x;
+                    targetTy = groundObj.transform.position.y + fallingRateDown;
+                    targetTz = hitPoint.z + offset.z;
+                    stepTx = transform.position.x + fallingRateSliding * (targetTx - transform.position.x);
+                    stepTy = transform.position.y + fallingRateDown * (targetTy - transform.position.y);
+                    stepTz = transform.position.z + fallingRateSliding * (targetTz - transform.position.z);
+                    if (stepTx <= fallingRateSliding)
+                    {
+                        stepTx = targetTx;
+                        isFallingTx = false;
+                    }
+                    if (stepTy <= fallingRateDown + groundObj.transform.position.y + .01f)
+                    {
+                        stepTy = targetTy;
+                        isFallingTy = false;
+                    }
+                    if (stepTz <= fallingRateSliding)
+                    {
+                        stepTz = targetTz;
+                        isFallingTz = false;
+                    }
+                    transform.position = new Vector3(stepTx, stepTy, stepTz);
+                    cardBeam.transform.position = new Vector3(stepTx, stepTy - 5, stepTz);
 
-                if (!hasLanded && stepTy <= fallingRateDown + groundPlane.transform.position.y + 1f)
-                {
-                    // Set the flag to indicate that we're playing audio
-                    isPlayingAudio = true;
-                    StartCoroutine(PlayAudioWithDelayCoroutine(cardDropSound, 0f));
-                    hasLanded = true;
-                    Debug.Log("Stopped Falling");
+                    // Gradually rotate flat
+                    targetR = Quaternion.Euler(0, -180, 0);
+                    float angle = Quaternion.Angle(transform.rotation, targetR);
+                    if (angle <= fallingRateRotating)
+                    {
+                        stepR = targetR;
+                        isFallingR = false;
+                    }
+                    else
+                    {
+                        stepR = Quaternion.Lerp(transform.rotation, targetR, fallingRateRotating);
+                    }
+                    transform.rotation = stepR;
+
+                    if (!hasLanded && stepTy <= fallingRateDown + groundObj.transform.position.y + 1f)
+                    {
+                        // Set the flag to indicate that we're playing audio
+                        isPlayingAudio = true;
+                        StartCoroutine(PlayAudioWithDelayCoroutine(cardDropSound, 0f));
+                        hasLanded = true;
+                    }
                 }
             }
         }
